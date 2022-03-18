e2059848168dd109025e6cb0b4f2b85124401b9f
gribdesbois <gribdesbois@gmail.com>
2022-03-18 14:35:51 +0100

    Article

---
+++ vite-project/src/components/Article/index.tsx
@@ -0,0 +1,29 @@
+import React, { useCallback } from 'react'
+import { useDispatch } from 'react-redux'
+import { Dispatch } from 'redux'
+
+type Props = {
+  article: IArticle
+  removeArticle: (article: IArticle) => null
+}
+
+const Article: React.FC<Props> = ({ article, removeArticle }) => {
+  const dispatch: Dispatch<any> = useDispatch()
+
+  const deleteArticle = useCallback(
+    (articleParam: IArticle) => dispatch(removeArticle(articleParam)),
+    [dispatch, removeArticle]
+  )
+
+  return (
+    <div className="Article">
+      <div>
+        <h1>{article.title}</h1>
+        <p>{article.body}</p>
+      </div>
+      <button onClick={() => deleteArticle(article)}>Delete</button>
+    </div>
+  )
+}
+
+export default Article
