841b87fc5ba9358f0e808d6d7f3ef643c58dd8d7
gribdesbois <gribdesbois@gmail.com>
2022-03-18 14:19:48 +0100

    AddArticle

---
+++ vite-project/src/components/AddArticle/index.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+type Props = {
+  saveArticle: (article: IArticle | any) => void
+}
+
+const AddArticle: React.FC<Props> = ({ saveArticle }) => {
+  const [article, setArticle] = useState<IArticle | {}>()
+
+  const handleArticleData = (e: React.FormEvent<HTMLInputElement>) => {
+    setArticle({
+      ...article,
+      [e.currentTarget.id]: e.currentTarget.value,
+    })
+  }
+
+  const addNewArticle = (e: React.FormEvent) => {
+    e.preventDefault()
+    saveArticle(article)
+  }
+
+  return (
+    <form onSubmit={addNewArticle} className="Add-article">
+      <input
+        type="text"
+        id="title"
+        placeholder="Title"
+        onChange={handleArticleData}
+      />
+      <input
+        type="text"
+        id="body"
+        placeholder="Description"
+        onChange={handleArticleData}
+      />
+      <button disabled={article === undefined}></button>
+    </form>
+  )
+}
+
+export default AddArticle
